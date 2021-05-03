86a150bb94a89d492b4558af44a8be31200e86ba
AlejandroGodinez <is714181@iteso.mx>
2021-05-03 11:25:27 -0500

    created the output json

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ exports.handler = async (event) => {
     let analyzeText = event.historial_clinico;
     
     const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-        version: '2020-08-01',
+        version: '2021-03-25',
         authenticator: new IamAuthenticator({
             apikey: process.env.apikey,
         }),
         serviceUrl: process.env.url,
     });
     
+    //se quiere analizar un string en base a sus keywords y entidades
     const analyzeParams = {
       'text': analyzeText,
       'features': {
@@ -28,12 +29,72 @@ exports.handler = async (event) => {
       }
     };
     
-    naturalLanguageUnderstanding.analyze(analyzeParams)
-    .then(analysisResults => {
-        console.log(JSON.stringify(analysisResults, null, 2));
-    })
-    .catch(err => {
-        console.log('error:', err);
+    let res = await naturalLanguageUnderstanding.analyze(analyzeParams);
+
+    // //obtener las palabras clave
+    let keywords = []
+    let keywordJson = {};
+    res.result.keywords.forEach(element => {
+      //se ingresa la palabra clave al arreglo
+      keywords.push(element.text);
+
+      let emotionInt = 0;
+      let emotion;
+
+      //obtener la llave que tenga la emocion mas representativa
+      for (const key in element.emotion) {
+        if(element.emotion[key] > emotionInt){
+          emotion = key
+          emotionInt = element.emotion[key]
+        } 
+      }
+    
+      keywordJson[element.text] = {
+        "sentimiento": element.sentiment.label,
+        "relevancia": element.relevance,
+        "repeticiones": element.count,
+        emotion
+      }
+      
+
     });
-    return analyzeParams;
+
+    // //obtener las entidades
+    let entities = []
+    let entitiesJson = {}
+    res.result.entities.forEach(element => {
+      //se ingresa al arreglo de entidades
+      entities.push(element.text);
+
+      let emotionInt = 0;
+      let emotion;
+
+      //obtener la llave que tenga la emocion mas representativa
+      for (const key in element.emotion) {
+        if(element.emotion[key] > emotionInt){
+          emotion = key
+          emotionInt = element.emotion[key]
+        } 
+      }
+
+      entitiesJson[element.text] = {
+        "tipo": element.type,
+        "sentimiento": element.sentiment.label,
+        "relevancia": element.confidence,
+        "emocion": emotion,
+        "repeticiones": element.count,
+        "porcentaje_confianza": element.confidence
+      }
+    })
+
+    // json a retornar
+    let json_return = {
+      "lenguaje_texto": res.result.language,
+      "palabras_clave": keywords,
+      "entidades": entities,
+      "palabras_clave_desc": keywordJson,
+      "entidades_desc": entitiesJson
+    }
+
+    return json_return;
 };
